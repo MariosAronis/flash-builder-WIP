32d7a8b3cd5c54b9ec08f8f22877a32cc48bb78f
AronisAt79 <mariosaronis@gmail.com>
2023-10-31 14:59:51 +0200

    various fixes

--- .github/scripts/deployTestnet.sh
+++ .github/scripts/deployTestnet.sh
@@ -1,25 +1,24 @@
 #!/bin/bash
 
-set -eux
+set -e
 
 BRANCH=$1
 USERNAME=$2
 VALUE=${USERNAME}-${BRANCH}
 
-echo $VALUE
-# VALUE=openvpn
 UserInstance=`aws ec2 describe-instances \
-    --filters "Name=tag:Name,Values=$VALUE" --output text \
-    --query 'Reservations[*].Instances[*].{InstanceId:InstanceId}'
-    # | jq '.[]'`
-Length=`echo $UserInstance | grep -c '^[^#]'`
+    --filters "Name=tag:Name,Values=$VALUE" --output json \
+    --query 'Reservations[*].Instances[*].{InstanceId:InstanceId}' \
+    | jq '.[]'`
 
-echo $UserInstance
-echo $Length
+Length=`echo $UserInstance | jq '. | length'`
 
 if [[ $Length -eq 1 ]] ; then
+    InstaceId=`echo $UserInstance | jq -r '.[] | ."InstanceId"'`
+    echo $InstanceId
     echo "Updating node"
 elif [[ $Length -gt 1 ]] ; then
+    ##TO DO
     echo "Multiple nodes, must kill all and restart"
 else
     echo "Deploying new node for user"
