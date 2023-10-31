9892f8d759927d7c0105528b06f12f8bfa979801
AronisAt79 <mariosaronis@gmail.com>
2023-10-31 16:59:27 +0200

    create_instance function

--- .github/scripts/deployTestnet.sh
+++ .github/scripts/deployTestnet.sh
@@ -1,10 +1,36 @@
 #!/bin/bash
 
-set -e
+set -ex
 
 BRANCH=$1
 USERNAME=$2
+# TRIGGER=$1
+# SHA=$4
+AMI="ami-0a588942e90cfecc9"
+SG="sg-06d9f0fd0b749c6ee"
+SUBNET=subnet-0160d2ef49ea42ecc
+VPC="	vpc-09831d1e37a601415"
 VALUE=${USERNAME}-${BRANCH}
+
+create_instance () {
+# Launch new instance
+aws ec2 run-instances \
+  --user-data "file://.github/scripts/cloud-init.sh" \
+  --image-id $AMI \
+  --count 1 \
+  --instance-type t3.2xlarge \
+  --key-name mariosee \
+  --security-group-ids $SG \
+  --subnet-id $SUBNET \
+  --block-device-mappings "[{\"DeviceName\":\"/dev/sda1\",\"Ebs\":{\"VolumeSize\":20,\"DeleteOnTermination\":true}}]" \
+#   --instance-initiated-shutdown-behavior terminate \
+  --tag-specification "ResourceType=instance,Tags=[{Key=Name,Value="FlashNode-Testnet-$USERNAME-$BRANCH"},{Key=Branch,Value="$BRANCH"}]" \
+  --metadata-options "InstanceMetadataTags=enabled"   
+}
+
+delete_instance () {
+pass
+}
 
 UserInstance=`aws ec2 describe-instances \
     --filters "Name=tag:Name,Values=$VALUE" --output json \
@@ -19,7 +45,8 @@ if [[ $Length -eq 1 ]] ; then
     echo "Updating node"
 elif [[ $Length -gt 1 ]] ; then
     ##TO DO
-    echo "Multiple nodes, must kill all and restart"
+    echo "Multiple nodes, must kill all and deploy a fresh one...proceeding!"
 else
     echo "Deploying new node for user"
+    create_instance
 fi
