706932c2c3f5b2fd2f2032c8beea64e68e9c42b6
AronisAt79 <mariosaronis@gmail.com>
2023-11-01 00:57:54 +0200

    test

--- .github/scripts/cloud-init.sh
+++ .github/scripts/cloud-init.sh
@@ -3,10 +3,10 @@
 set -ex
 
 # Install pkgs
-sudo apt-get update
-sudo apt get install -y net-tools git
-sudo apt install software-properties-common
-sudo add-apt-repository --yes --update ppa:ansible/ansible
-sudo apt install ansible -y
+apt-get update
+apt get install -y net-tools git
+apt install software-properties-common
+add-apt-repository --yes --update ppa:ansible/ansible
+apt install ansible -y
 
 ansible-galaxy role install gantsign.golang
