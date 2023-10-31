4b2063fc40dfe39942f40c8a0627e32f6f8ebc79
AronisAt79 <mariosaronis@gmail.com>
2023-11-01 01:27:37 +0200

    no sudo

--- .github/scripts/cloud-init.sh
+++ .github/scripts/cloud-init.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
-echo "THIS IS A PLACEHOLDER"
+set -ex
+
+# Install pkgs
+apt-get update
+apt get install -y net-tools git
+apt install software-properties-common
+add-apt-repository --yes --update ppa:ansible/ansible
+apt install ansible -y
+
+ansible-galaxy role install gantsign.golang
