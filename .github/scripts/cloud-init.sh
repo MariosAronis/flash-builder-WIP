ce69f8ac8f7a0647f4892445dd5dfd7f46567c72
AronisAt79 <mariosaronis@gmail.com>
2023-11-01 13:38:38 +0200

    cloud-init fix permissions

--- .github/scripts/cloud-init.sh
+++ .github/scripts/cloud-init.sh
@@ -11,8 +11,9 @@ apt-get install software-properties-common
 add-apt-repository --yes --update ppa:ansible/ansible
 apt-get install ansible -y
 hostnamectl set-hostname $HOSTNAME
-git clone https://github.com/MariosAronis/FlashNodes-WIP.git ~/Flashnodes
+git clone https://github.com/MariosAronis/FlashNodes-WIP.git /home/ubuntu/Flashnodes
 
 ansible-galaxy role install --roles-path=/home/ubuntu/Flashnodes/ansible gantsign.golang
 
+chown -R ubuntu:ubuntu /home/ubuntu/Flashnodes
 ansible-playbook  /home/ubuntu/Flashnodes/ansible/provision-flashnode.yml --skip-tags remote
