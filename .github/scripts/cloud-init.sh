24ca3ab120e8848567bb94919ee67c133e88f5fe
AronisAt79 <mariosaronis@gmail.com>
2023-11-01 14:57:14 +0200

    yq is not needed

--- .github/scripts/cloud-init.sh
+++ .github/scripts/cloud-init.sh
@@ -6,7 +6,7 @@ HOSTNAME="__HOSTNAME__"
 
 # Install pkgs
 apt-get update
-apt-get install -y net-tools git yq
+apt-get install -y net-tools git
 apt-get install software-properties-common
 add-apt-repository --yes --update ppa:ansible/ansible
 apt-get install ansible -y
