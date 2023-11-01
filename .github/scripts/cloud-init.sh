66e37d713d212ae7bd48a672203532689176e30e
AronisAt79 <mariosaronis@gmail.com>
2023-11-01 12:38:30 +0200

    add HOSTNAME variable, set hostname, install ansible and golang role, exec play

--- .github/scripts/cloud-init.sh
+++ .github/scripts/cloud-init.sh
@@ -2,11 +2,18 @@
 
 set -ex
 
+HOSTNAME="__HOSTNAME__"
+
 # Install pkgs
 apt-get update
 apt-get install -y net-tools git
 apt-get install software-properties-common
 add-apt-repository --yes --update ppa:ansible/ansible
 apt-get install ansible -y
+hostnamectl set-hostname $HOSTNAME
+git clone https://github.com/MariosAronis/FlashNodes-WIP.git ~/Flashnodes
 
-ansible-galaxy role install gantsign.golang
+ansible-galaxy role install --roles-path=$HOME/Flashnodes/ansible gantsign.golang
+
+
+ansible-playbook  ansible/provision-flashnode.yml --skip-tags remote
