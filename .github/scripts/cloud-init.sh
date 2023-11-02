75d698520e868770503400aac6eaa69c868054cf
AronisAt79 <mariosaronis@gmail.com>
2023-11-02 16:04:41 +0200

    cloud init typos

--- .github/scripts/cloud-init.sh
+++ .github/scripts/cloud-init.sh
@@ -15,5 +15,5 @@ hostnamectl set-hostname $HOSTNAME
 # Clone the Flashnodes repo
 su -P ubuntu -c "git clone https://github.com/MariosAronis/FlashNodes-WIP.git /home/ubuntu/Flashnodes"
 #Install gantsign.golang role
-su -p ubuntu -c "ansible-galaxy role install gantsign.golang"
-su -p ubuntu -c "ansible-playbook /home/ubuntu/Flashnodes/ansible/provision-flashnode.yml --skip-tags remote"
+su -P ubuntu -c "ansible-galaxy role install gantsign.golang"
+su -P ubuntu -c "ansible-playbook /home/ubuntu/Flashnodes/ansible/provision-flashnode.yml --skip-tags remote"
