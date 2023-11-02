3c3b4cb5275658991deef671c9993e70a60274fd
AronisAt79 <mariosaronis@gmail.com>
2023-11-02 13:18:10 +0200

    cloud init almost final

--- .github/scripts/cloud-init.sh
+++ .github/scripts/cloud-init.sh
@@ -10,8 +10,6 @@ apt-get install -y net-tools git
 apt-get install software-properties-common
 add-apt-repository --yes --update ppa:ansible/ansible
 apt-get install ansible -y
-# ansible-galaxy role install gantsign.golang
-
 # Set Hostname to Flashnode-Testnet-$GHUSERNAME-BRANCHNAME
 hostnamectl set-hostname $HOSTNAME
 
@@ -19,6 +17,4 @@ hostnamectl set-hostname $HOSTNAME
 su -P ubuntu -c "git clone https://github.com/MariosAronis/FlashNodes-WIP.git /home/ubuntu/Flashnodes"
 #Install gantsign.golang role
 su -p ubuntu -c "ansible-galaxy role install --roles-path=/home/ubuntu/Flashnodes/ansible gantsign.golang"
-# su -p ubuntu -c "ansible-galaxy role install gantsign.golang"
-# su -p ubuntu -c "
-ansible-playbook /home/ubuntu/Flashnodes/ansible/provision-flashnode.yml --skip-tags remote
+su -p ubuntu -c "ansible-playbook /home/ubuntu/Flashnodes/ansible/provision-flashnode.yml --skip-tags remote"
